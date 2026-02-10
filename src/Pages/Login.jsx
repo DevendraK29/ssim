ebccb25bd0935d5071040f70fe7f3858e0d078ef
DevendraK29 <devendrakamble2907@gmail.com>
2026-02-10 19:46:01 +0530

    Login Page Created

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,63 @@
+import "../styles/Login.css"
+import { useState } from "react";
+
+function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleLogin = () => {
+    if (username === "admin" && password === "admin123") {
+      alert("Login successful!");
+      // later: navigate to dashboard
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <div className="left">
+        <div className="mock">
+          <h1>SSIM</h1>
+          <p>Stationery Store Inventory Management</p>
+          <p>Manage sales, stock, and reports easily.</p>
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="login-box">
+          <h2>Sign in to SSIM</h2>
+
+          {error && <div className="error">Invalid email or password</div>}
+
+          <input
+            type="text"
+            className="field"
+            placeholder="Email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <input
+            type="password"
+            className="field"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="row">
+            <a href="#">Forgot Password?</a>
+          </div>
+
+          <button className="btn" onClick={handleLogin}>
+            Sign in
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
